87f1b81488570f342c6f18edbce3d2f672f70d3a
Mario Gallo <marioffgallo@hotmail.com>
2021-01-06 11:07:48 -0300

    Criada validações no formCadastro

---
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'dio-cadastro-filmes',
+  templateUrl: './cadastro-filmes.component.html',
+  styleUrls: ['./cadastro-filmes.component.scss']
+})
+export class CadastroFilmesComponent implements OnInit {
+
+  cadastro: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  get f() {
+    return this.cadastro.controls;
+  }
+
+  ngOnInit(): void {
+
+    this.cadastro = this.fb.group({
+      titulo: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(256)]],
+      urlFoto: ['', [Validators.minLength(2)]],
+      dtLancamento: ['', [Validators.required]],
+      descricao: [''],
+      nota: [0, [Validators.required, Validators.min(0), Validators.max(10)]],
+      urlIMDb: ['', [Validators.minLength(10)]],
+      genero: ['', [Validators.required]],
+    });
+
+  }
+
+  salvar(): void {
+    this.cadastro.markAllAsTouched();
+    
+    if(this.cadastro.invalid){
+      return;
+    }
+
+    alert('SUCESSO!!\n\n' + JSON.stringify(this.cadastro.value, null, 4));
+  }
+
+  reiniciarForm(): void{
+    this.cadastro.reset;
+  }
+}
